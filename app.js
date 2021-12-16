10862c2b01a7a167673a67980c352a24865ae615
Aurix16 <dawn.idoko@gmail.com>
2021-12-16 10:43:11 -0600

    Finished creating routes

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 // Importing global http module
 const http = require('http');
-
+const routes = require('./routes');
 
 // Creating a server
-const server = http.createServer((req, res)=>{
-    // console.log(req.url, req.method, req.headers);
-    // process.exit(); // exit server listening
-    
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
