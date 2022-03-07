76a068e788fbfb0367e76f5d5b18cc9ac38662ba
Aurix16 <dawn.idoko@gmail.com>
2022-03-07 06:30:25 -0600

    Changed Folder Format and Code structure

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // Importing global http module
-const http = require('http');
-
+const path = require('path');
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continue to the next middleware in line
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('');
-  });
-
 // Creating a server
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
